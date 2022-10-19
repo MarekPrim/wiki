b04e6d9926e8cd7c89cc1065d8e0606397c849c2
Marek <59203712+MarekPrim@users.noreply.github.com>
2022-10-19 15:30:46 +0200

    first commit

---
+++ src/components/Image.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {onMounted, ref} from 'vue'
+
+defineProps({
+  img: String
+})
+
+onMounted(() => {
+  alert("Image mounted");
+})
+
+</script>
+
+<template>
+
+
+      <div v-if="img.startsWith('u')">
+         <p>{{img}}</p>
+      </div>
+      <div v-else>
+         <p>QUOI ! Ca ne commence pas par U</p>
+      </div>
+
+</template>
+<style scoped>
+.image:hover{
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
