3044188f4331bb06892d09a12102dcd8e09c181d
Marek <59203712+MarekPrim@users.noreply.github.com>
2022-11-27 18:26:12 +0100

    added sidebar on page

--- frontend/src/components/Page.vue
+++ frontend/src/components/Page.vue
@@ -7,12 +7,27 @@ const name = route.params.name;
 </script>
 
 <template>
+  <div>
+    <div class="sidebar bg-slate-300 text-black">
+				<div class="logo">
+					<a href="/"><img src='../../assets/logo.png' alt="logo"></a>
+                    <p class="text-xs">Gracefully generated logo by <a href="https://openai.com/dall-e-2/">DALL-E AI</a>. Check out their work !</p>
+				</div>
+				<div class="navigation ">
+					<ul>
+						<li><a href="pages">Main page</a></li>
+						<li><a href="#">Contents</a></li>
+					</ul>
+				</div>
+
+			
+			</div>
   <article class="bg-slate-50" id="view">
     <div class="article">
       <a :href="'/edit/' + name" class="text-left">Edit</a>
       <Markdown :name="name" />
     </div>
-  </article>
+  </article></div>
 </template>
 <style scoped>
 #view {
