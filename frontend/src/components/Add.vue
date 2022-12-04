62473393a3ec1c2f54624ec473c14c053714e93c
MarekPrim <59203712+MarekPrim@users.noreply.github.com>
2022-12-04 20:28:06 +0100

    hide certain element on print

--- frontend/src/components/Add.vue
+++ frontend/src/components/Add.vue
@@ -1,20 +1,19 @@
 <script>
 import FrontMatter from 'front-matter';
 import { marked } from 'marked';
-import axios from 'axios';
-import feathers from '@feathersjs/client'
-import io from 'socket.io-client'
+import feathers from '@feathersjs/client';
+import io from 'socket.io-client';
 
 const app = feathers();
-    const socket = io("http://localhost:3030",{
-        path: '/gallery-socket-io',
-        transports: ["websocket"], // mandatory with Vite
-    })
-    app.configure(feathers.socketio(socket))
+const socket = io('http://localhost:3030', {
+  path: '/gallery-socket-io',
+  transports: ['websocket'], // mandatory with Vite
+});
+app.configure(feathers.socketio(socket));
 // import Vue from 'vue';
 marked.setOptions({
-        breaks: true
-})
+  breaks: true,
+});
 export default {
   name: 'AddPage',
   props: ['name'],
@@ -44,10 +43,10 @@ short_description: <your short description>
   computed: {
     compiledMarkdown() {
       let data = FrontMatter(this.allContent);
-      console.log("data_boyd", data.body);
+      console.log('data_boyd', data.body);
 
       let md = marked(data.body);
-      console.log("md", md);
+      console.log('md', md);
       return md;
     },
   },
@@ -64,21 +63,28 @@ short_description: <your short description>
           author: attributes.author,
           description: attributes.short_description,
         };
-        if(articleData.author=="<your name>" || articleData.description=="<your short description>"){
-          alert("Please fill the author and the description");
+        if (
+          articleData.author == '<your name>' ||
+          articleData.description == '<your short description>'
+        ) {
+          alert('Please fill the author and the description');
           return;
         }
         console.log(articleData);
-        socket.emit('create', '/api/pages', {
-          id: this.$props.name,
-          author: articleData.author,
-          markdown : articleData.content,
-          description: articleData.description,
-}, (error, message) => {
-  console.log('Todo created', message);
-  console.error(error);
-});
-
+        socket.emit(
+          'create',
+          '/api/pages',
+          {
+            id: this.$props.name,
+            author: articleData.author,
+            markdown: articleData.content,
+            description: articleData.description,
+          },
+          (error, message) => {
+            console.log('Todo created', message);
+            console.error(error);
+          }
+        );
       });
 
       // Then proceed to using articleData object as needed
@@ -89,37 +95,40 @@ short_description: <your short description>
 
 <template>
   <div>
-  <a>Add the {{ name }} page with the editor below</a>
-  <div id="editor-form-container" class="flex-col align-middle justify-center p-6">
-    <div id="button-container">
+    <a>Add the {{ name }} page with the editor below</a>
+    <div
+      id="editor-form-container"
+      class="flex-col align-middle justify-center p-6"
+    >
+      <div id="button-container" class="no-print">
+        <button
+          v-on:click="previewMarkdown = !previewMarkdown"
+          class="uk-button uk-button-primary uk-float-left"
+        >
+          {{ previewMarkdown ? 'Edit Article' : 'Preview Article' }}
+        </button>
+      </div>
+      <div id="editor-container" class="p-6">
+        <textarea
+          v-if="!previewMarkdown"
+          v-model="allContent"
+          class="lock p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          id="markdown-editor"
+        ></textarea>
+        <div
+          v-else
+          class="preview my-6 mx-auto p-6 bg-slate-200 text-green-700 w-1/2"
+          v-html="compiledMarkdown"
+        ></div>
+      </div>
       <button
-        v-on:click="previewMarkdown = !previewMarkdown"
-        class="uk-button uk-button-primary uk-float-left"
+        v-on:click="postServer"
+        class="uk-button uk-button-primary no-print"
       >
-        {{ previewMarkdown ? 'Edit Article' : 'Preview Article' }}
+        Envoyer les mises à jour sur le serveur
       </button>
     </div>
-    <div id="editor-container" class="p-6">
-      <textarea
-        v-if="!previewMarkdown"
-        v-model="allContent"
-        class="
-        lock p-2.5 w-full text-sm 
-        text-gray-900 bg-gray-50 rounded-lg border
-        border-gray-300 focus:ring-blue-500 
-        focus:border-blue-500 dark:bg-gray-700 
-        dark:border-gray-600 dark:placeholder-gray-400 
-        dark:text-white dark:focus:ring-blue-500 
-        dark:focus:border-blue-500"
-        id="markdown-editor"
-      ></textarea>
-      <div v-else class="preview my-6 mx-auto p-6 bg-slate-200 text-green-700 w-1/2" v-html="compiledMarkdown"></div>
-    </div>
-    <button v-on:click="postServer" class="uk-button uk-button-primary">
-      Send this to the server
-    </button>
   </div>
-</div>
 </template>
 
 <style scoped>
